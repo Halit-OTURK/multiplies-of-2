75fdd0b2c3bc1e31bd28e2c39026d3544bb7b8aa
halithamza <halithamza0920@gmail.com>
2022-10-23 11:56:03 +0300

    Create Ikinin_Kuvvetleri.java
    www.patika.dev multipies of 2

---
+++ Ikinin_Kuvvetleri.java
@@ -0,0 +1,13 @@
+package donguler;
+import java.util.Scanner;
+public class Ikinin_Kuvvetleri {
+    public static void main(String[] args) {
+        int a;
+        Scanner ktsy= new Scanner(System.in);
+        System.out.print("Sayı Giriniz:");
+        a= ktsy.nextInt();
+       for( int i=1; i<1000; i *=2){
+        System.out.println(i); }
+
+    }
+}
